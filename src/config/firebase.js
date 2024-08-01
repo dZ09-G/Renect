9848e5e49f1632ed5fb05bd33887153d222af800
geeth-chand <geethchand49@gmail.com>
2024-08-01 16:15:30 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyComvWqbHZ-6oB8E6cN_tvOKWkgDQr1NJE",
   authDomain: "social-media-react-1eae8.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
   appId: "1:181015686121:web:3ebb119e9b31572a28f978"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth=getAuth(app);
